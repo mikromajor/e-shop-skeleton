7fb448e7c5211daf493fbf4cf3c136a2a541d613
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-07-03 01:45:34 +0200

    add method delete to Brand field

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -12,5 +12,14 @@ class BrandController {
     const brands = await Brand.findAll();
     return res.json(brands);
   }
+
+  async delete(req, res) {
+    const { name } = req.body; //DELETE has body
+    await Brand.destroy({
+      where: { name: name },
+    });
+    const brand = await Brand.findAll();
+    return res.json(brand);
+  }
 }
 module.exports = new BrandController();
--- server/routes/brandRouter.js
+++ server/routes/brandRouter.js
@@ -1,9 +1,19 @@
 const Router = require("express");
 const router = new Router();
 const brandController = require("../controllers/brandController");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
-router.post("/", brandController.create);
+router.post(
+  "/",
+  checkRoleMiddleware("ADMIN"),
+  brandController.create
+);
 router.get("/", brandController.getAll);
+router.delete(
+  "/",
+  checkRoleMiddleware("ADMIN"),
+  brandController.delete
+);
 // router.delete("/");
 
 module.exports = router;
