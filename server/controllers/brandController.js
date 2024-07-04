e9b73a2b9a1dc3bf2cd64ff1a917f1ed06cde6cc
Alex Solianyk <72755893+mikromajor@users.noreply.github.com>
2024-07-04 20:13:43 +0200

    noted helps with req

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -3,22 +3,30 @@ const ApiError = require("../error/ApiError");
 
 class BrandController {
   async create(req, res) {
+    //POST
     const { name } = req.body;
     const brand = await Brand.create({ name });
     return res.json(brand);
   }
 
   async getAll(req, res) {
+    //GET
+    // /brand?name=bmw&id=10
+    // const {name, id}=req.query
+
     const brands = await Brand.findAll();
     return res.json(brands);
   }
 
   async getOne(req, res) {
+    // /brand/bmw
+    // "/:name"
+    //  req.params.name === "bmw"
     const { name } = req.params;
-    const device = await Brand.findOne({
+    const brand = await Brand.findOne({
       where: { name },
     });
-    return res.json(device);
+    return res.json(brand);
   }
 
   async delete(req, res) {
